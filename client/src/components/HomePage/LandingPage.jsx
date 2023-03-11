ed42939384eb0899c1127b4ab5d486ccc2e607aa
vijay4145 <svijay4145@gmail.com>
2023-03-11 23:08:02 +0530

    updated quizs

--- client/src/components/HomePage/LandingPage.jsx
+++ client/src/components/HomePage/LandingPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Question } from "./Questions";
 
 export const LandingPage = () => {
   return (
     <>
-    <h1>uihfdsyugfsu</h1>
+    <Question/>
     </>
   )
 }
