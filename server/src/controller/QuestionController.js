3dbf5f06395dd859bf043315c50826fcc0576fa1
vijay4145 <svijay4145@gmail.com>
2023-03-12 00:43:04 +0530

    update question result percentage

--- server/src/controller/QuestionController.js
+++ server/src/controller/QuestionController.js
@@ -47,9 +47,20 @@ exports.question = {
 
     resultPost: (req, res) =>{
         const answerKeyWeb = [0,0,0,0,0,0,0,0,0,0]
-        const answerKeyAndroid = [1,1,1,1,1,]
-        const data = req.body;
-        res.status(200).json(data)
+        const answerKeyAndroid = [1,1,1,1,1,1,1,1,1,1];
+        let webScore = 0;
+        let androidScore = 0;
+        let userResponse = req.body.answer;
+        for(let i=0; i<10; i++){
+            if(answerKeyAndroid[i] === userResponse[i]) androidScore++;
+            if(answerKeyWeb[i] === userResponse[i]) webScore++;
+        }
+        res.status(200).json({
+            WebScore: webScore,
+            androidScore: androidScore,
+            androidInterestPercentage: androidScore*10,
+            webInterestPercentage: webScore*10
+        })
     }
 }
 
